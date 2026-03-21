45835d066b89211b64a040ccf7be91f526a4ff12
Daniel da Silva <dsilvaba@pm.me>
2026-03-21 15:25:19 +0100

    Added posix serial

---
+++ hal/posix/posix_serial_hal/serial_hal.cpp
@@ -0,0 +1,56 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <vector>
+
+#include "result.hpp"
+#include "serial_hal.hpp"
+
+namespace serial {
+SerialHal::SerialHal(const char *device, int baud_rate) {
+        fd = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
+
+        termios tty{};
+        tcgetattr(fd, &tty);
+        cfsetispeed(&tty, baud_rate);
+        cfsetospeed(&tty, baud_rate);
+        tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+        tty.c_cflag |= CLOCAL | CREAD;
+        tty.c_cflag &= ~(PARENB | PARODD | CSTOPB | CRTSCTS);
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+        tty.c_lflag = 0;
+        tty.c_oflag = 0;
+        tty.c_cc[VMIN] = 0;
+        tty.c_cc[VTIME] = 0;
+        tcsetattr(fd, TCSANOW, &tty);
+}
+
+SerialHal::~SerialHal() {
+        if (fd >= 0)
+                close(fd);
+}
+
+result::Result<bool> SerialHal::send(std::span<const uint8_t> data) {
+        const auto written = write(fd, data.data(), data.size());
+        if (written < 0)
+                return result::err("failed to write to serial port");
+        return result::ok();
+}
+
+void SerialHal::on_receive(ReceiveCallback cb) {
+        receive_callback = std::move(cb);
+}
+
+result::Result<bool> SerialHal::loop() {
+        std::vector<uint8_t> data(BUF_SIZE);
+        const auto length = read(fd, data.data(), BUF_SIZE);
+        if (length < 0)
+                return result::err("failed to read from serial port");
+        if (length == 0)
+                return result::ok();
+        data.resize(length);
+        if (receive_callback)
+                receive_callback(data);
+        return result::ok();
+}
+} // namespace serial
---
+++ hal/posix/posix_serial_hal/serial_hal.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <cstdint>
+#include <functional>
+#include <termios.h>
+
+#include "i_serial_hal.hpp"
+#include "result.hpp"
+
+namespace serial {
+class SerialHal : public ISerialHal {
+      public:
+        explicit SerialHal(const char *device, int baud_rate = B115200);
+        ~SerialHal();
+
+        SerialHal(const SerialHal &) = delete;
+        SerialHal &operator=(const SerialHal &) = delete;
+
+        result::Result<bool> send(std::span<const uint8_t> data) override;
+        void on_receive(ReceiveCallback cb) override;
+        result::Result<bool> loop() override;
+
+      private:
+        int fd;
+        ReceiveCallback receive_callback;
+        static constexpr auto BUF_SIZE = 1024;
+};
+} // namespace serial
