0c8b63af80cd6dbac45e6478f4b513a60efd7d0c
Daniel da Silva <dsilvaba@pm.me>
2026-04-05 11:53:58 +0200

    Added direct transporter

---
+++ transport/transporter/direct_transporter/direct_transporter.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "base_transporter.hpp"
+#include "i_logger.hpp"
+#include "logger.hpp"
+#include "result.hpp"
+#include "transporter.hpp"
+
+namespace transport {
+template <Transporter T> class DirectTransporter : public BaseTransporter {
+      public:
+        explicit DirectTransporter(T &transporter) : transporter(transporter) {
+                transporter.set_receiver([this](result::Result<Data> result) {
+                        const auto callback_result = try_callback(result);
+                        if (callback_result.failed()) {
+                                logging::logger().println(
+                                    logging::LogLevel::Error, TAG,
+                                    result.error());
+                                return;
+                        }
+                });
+        }
+
+        result::Result<bool> send(Data &&data) override {
+                return transporter.send(std::move(data));
+        }
+
+        MTU get_mtu() const override {
+                return transporter.get_mtu();
+        }
+
+      private:
+        static constexpr auto TAG = "DirectTransporter";
+        T &transporter;
+};
+} // namespace transport
